7ff58cde11ec290af5d0dbc1abaa46f658d8b832
JoaoLaimer <jvlaimer@gmail.com>
2023-02-26 21:49:09 -0300

    commit

---
+++ Slide2Exercicio4/README.md
@@ -0,0 +1,1 @@
+tentei fazer o realloc fake aqui com o memcpy e deu certo surpreendentemente, menos o leak de memoria que ta dando e eu nao sei como resolver então isso nao é legal pq por mais que o programa funcione o esse leak destroi tudo. Mas mtos outros problemas que o dr.memory apontava eu consegui resolver, entao isso é legal eu acho.
--- Slide2Exercicio4/src/main.c
+++ Slide2Exercicio4/src/main.c
@@ -1,18 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+void* reallocFake(void *pointer, size_t newSize);
 int main(int argc, char const *argv[])
 {
     
     void *pointer = malloc(3*sizeof(int));
-    void *qointer = malloc(3*sizeof(int));
-    *(int *)pointer = 1;
-        pointer++;
-    *(int *)pointer = 2;
-        pointer++;
+    void *inicio;
+
+    inicio = pointer;
+
     *(int *)pointer = 3;
-        pointer++;
+        pointer += sizeof(int);
+    *(int *)pointer = 2;
+        pointer += sizeof(int);
+    *(int *)pointer = 1;
+
+    pointer = inicio; 
+
+    
+    printf("%d\n",*(int *)pointer);
+        pointer += sizeof(int);
+    printf("%d\n",*(int *)pointer);
+        pointer += sizeof(int);
+    printf("%d\n",*(int *)pointer);
+        
     
 
+    pointer = inicio;
+    
+    pointer = reallocFake(pointer,4*sizeof(int));
+
+        printf("%d\n",*(int *)pointer);
+        pointer += sizeof(int);
+        printf("%d\n",*(int *)pointer);
+        pointer += sizeof(int);
+        printf("%d\n",*(int *)pointer);
+        pointer += sizeof(int);
+    
+
+    *(int *)pointer = 4;
+    printf("%d\n",*(int *)pointer);
+    
+    
+    free(inicio);
     return 0;
+}
+
+void* reallocFake(void *pointer, size_t newSize)
+{
+    void *newPointer = malloc(newSize);
+
+    memcpy(newPointer,pointer,3*sizeof(int));
+
+    return newPointer;
 }
